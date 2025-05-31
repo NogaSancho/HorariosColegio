e11808bb0b6fdc9f2934b2874e79fe20ed9ebfef
ESH2007 <milaroco3004@gmail.com>
2025-05-30 19:56:56 -0500

    Añadiendo el footer para decorar (luego le voy a encontrar una utilidad o borrarlo jaja)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header.jsx'
 import HorariosForm from './HorariosForm.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <HorariosForm />
+      <Footer />
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Bootstrap/css/bootstrap.min.css';
+import './Bootstrap/js/bootstrap.bundle.min.js';
+import './Bootstrap/js/color-modes.js';
+
+function Footer() {
+    return (
+        <div className="container">
+            <footer className="py-3 my-4 border-top">
+                <p className="text-center text-body-secondary">
+                    Horario Chevere 🗿🗿
+                </p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
